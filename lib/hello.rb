0609c0f1ee9c7ed4d5111c8fbf01a80cb8235111
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-13 21:16:41 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(names)
+<<<<<<< HEAD
   if block_given?
     counter = 0
 
@@ -10,6 +11,15 @@ def hello_t(names)
   else
     puts "Hey! No block was given!"
   end
+=======
+  counter = 0
+
+  while counter < names.length do
+    yield(names)
+    counter += 1
+  end
+  names
+>>>>>>> 17f1292a8a99a5bad2809f34bc53c0c781c21c22
 end
 
 # call your method here!
